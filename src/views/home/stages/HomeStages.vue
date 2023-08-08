42d60ae1f56b066705788a1a70069bc4beba9859
Sabyrzhan Kadir <sab.sabyr@gmail.com>
2023-08-08 23:17:38 +0500

    font size fix

--- src/views/home/stages/HomeStages.vue
+++ src/views/home/stages/HomeStages.vue
@@ -8,28 +8,28 @@
       <div class="stages__bignum left">1</div>
       <div class="stages__line"></div>
       <div class="stages__textblock">
-        <div class="stages__rainbowtitle">Обсуждение проекта</div>
-        <div class="stages__text">Во время нашего созвона мы задаем вам вопросы чтобы получить всю необходимую и важную информацию о вашем бизнесе и лучше понять его цели и задачи.</div>
+        <div class="stages__rainbowtitle">Связь с нами</div>
+        <div class="stages__text">После связи с нами и ознакомления с вашим продуктом или услугой мы организуем созвон</div>
       </div>
-      <div class="stages__textblock">
+      <div class="stages__textblock left">
         <div class="stages__rainbowtitle">Обсуждение проекта</div>
         <div class="stages__text">Во время нашего созвона мы задаем вам вопросы чтобы получить всю необходимую и важную информацию о вашем бизнесе и лучше понять его цели и задачи.</div>
       </div>
       <div class="stages__bignum">2</div>
       <div class="stages__bignum left">3</div>
       <div class="stages__textblock">
-        <div class="stages__rainbowtitle">Обсуждение проекта</div>
-        <div class="stages__text">Во время нашего созвона мы задаем вам вопросы чтобы получить всю необходимую и важную информацию о вашем бизнесе и лучше понять его цели и задачи.</div>
+        <div class="stages__rainbowtitle">Подключение программистов / дизайнеров / маркетологов</div>
+        <div class="stages__text">После получения всей необходимой информации, мы распределяем работу между нашей командой для эффективного и планомерного выполнения задач.</div>
       </div>
-      <div class="stages__textblock">
-        <div class="stages__rainbowtitle">Обсуждение проекта</div>
-        <div class="stages__text">Во время нашего созвона мы задаем вам вопросы чтобы получить всю необходимую и важную информацию о вашем бизнесе и лучше понять его цели и задачи.</div>
+      <div class="stages__textblock left">
+        <div class="stages__rainbowtitle">Процесс разработки</div>
+        <div class="stages__text">Перед началом работ мы устанавливаем сроки, в которых подробно описываем все этапы процесса разработки.</div>
       </div>
       <div class="stages__bignum">4</div>
       <div class="stages__bignum left">5</div>
       <div class="stages__textblock">
-        <div class="stages__rainbowtitle">Обсуждение проекта</div>
-        <div class="stages__text">Во время нашего созвона мы задаем вам вопросы чтобы получить всю необходимую и важную информацию о вашем бизнесе и лучше понять его цели и задачи.</div>
+        <div class="stages__rainbowtitle">Готовый проект</div>
+        <div class="stages__text">После слаженной и комплексной работы наших сотрудников вы получаете готовый проект с продуманным дизайном, грамотным маркетингом и безупречной технической работы наших программистов</div>
       </div>
     </div>
   </div>
@@ -70,6 +70,7 @@
     justify-content: center;
     column-gap: 30px;
 
+
     .stages__line {
       grid-column: 2;
       grid-row: 1 / span 5;
@@ -90,12 +91,20 @@
       }
     }
 
+    .stages__textblock.left {
+      text-align: right;
+      float: right;
+    }
+
     .stages__rainbowtitle {
       font-family: 'Inter';
-      font-size: 1.5rem;
+      font-size: 2rem;
       font-style: normal;
       font-weight: 400;
       line-height: normal;
+      max-width: 400px;
+
+      margin-top: 20px;
 
       background: linear-gradient(167deg, #000C99 0%, #009234 100%);
       background-clip: text;
@@ -104,7 +113,8 @@
     }
 
     .stages__text {
-      width: 558px;
+      width: 400px;
+      font-size: 1.5rem;
     }
   }
 }
