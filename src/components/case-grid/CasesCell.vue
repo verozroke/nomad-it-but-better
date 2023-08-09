5b1b5a9c8e62ceb4e39a4f893198b999c5ae4887
Maulen <mac@MacBook-Air-mak.Dlink>
2023-08-09 13:38:46 +0600

    [инфо] сверстал первые блоки в страницах Кейсов для дизайна и разработки

--- src/components/case-grid/CasesCell.vue
+++ src/components/case-grid/CasesCell.vue
@@ -2,7 +2,9 @@
   <div class="block">
     <div class="block-overlay">
       <div class="overlay-title">Проект "Kourgau City"</div>
-      <a href="#" target="_blank"><v-btn variant="outlined" class="overlay-button">Ознакомиться</v-btn></a>
+      <a href="#" target="_blank">
+        <v-btn variant="outlined" class="overlay-button">Ознакомиться</v-btn>
+      </a>
     </div>
     <img :src="`/gallery/${src}`" alt="Gallery Photo" />
   </div>
@@ -10,8 +12,8 @@
 
 <script setup lang="ts">
 defineProps({
-  src: String
-})
+  src: String,
+});
 </script>
 
 <style lang="scss" scoped>
@@ -21,14 +23,11 @@ defineProps({
   max-width: 50%;
   position: relative;
 
-
-
   &:hover {
     .block-overlay {
       opacity: 1;
-      transition: .4s;
+      transition: 0.4s;
     }
-
   }
 
   &-overlay {
@@ -41,12 +40,12 @@ defineProps({
     text-align: center;
     height: 100%;
     font-size: 24px;
-    font-family: 'Montserrat', sans-serif;
+    font-family: "Montserrat", sans-serif;
     letter-spacing: 1px;
     font-weight: 400;
     opacity: 0;
     position: absolute;
-    transition: .4s;
+    transition: 0.4s;
     background-color: rgba(0, 0, 0, 0.5);
     top: 0;
     left: 0;
@@ -66,19 +65,16 @@ a {
   color: #fff !important;
 }
 
-
 @media only screen and (max-width: 750px) {
   .block {
     width: 100%;
     max-width: 100%;
     height: 100vw;
-
   }
 }
 
 @media only screen and (max-width: 436px) {
   .block {
-
     &-overlay {
       padding: 0 40px;
       font-size: 16px;
--- src/views/cases-dev/CasesDevView.vue
+++ src/views/cases-dev/CasesDevView.vue
@@ -1,14 +1,58 @@
+<script setup lang="ts">
+import CasesGrid from "../../components/case-grid/CasesGrid.vue";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  window.scrollTo(0, 0);
+});
+</script>
+
 <template>
+  <main class="main">
+    <div class="main__container">
+      <h1 class="main__title">Кейсы програмирования</h1>
+
+      <div class="main__list">
+        <p class="main__item">создание полноценных веб-сайтов</p>
+        <p class="main__item">front-end</p>
+        <p class="main__item">верстка лендингов</p>
+        <p class="main__item">it-консультация</p>
+        <p class="main__item">создание внутренних корпаративных веб-платформ</p>
+        <p class="main__item">back-end</p>
+        <p class="main__item">создание ботов</p>
+      </div>
+    </div>
+  </main>
+
   <CasesGrid />
 </template>
 
-<script setup lang="ts">
-import CasesGrid from '../../components/case-grid/CasesGrid.vue'
-import { onMounted } from 'vue'
+<style scoped lang="scss">
+.main {
+  &__title {
+    margin-top: 1.25rem;
+    padding-left: 0.5rem;
 
-onMounted(() => {
-  window.scrollTo(0, 0)
-})
-</script>
+    font-size: 3.125rem;
+    font-weight: 700;
+    line-height: normal;
+  }
 
-<style scoped></style>
+  &__list {
+    padding-top: 8.375rem;
+    padding-bottom: 7.25rem;
+    padding-inline: 0.5rem;
+
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1rem;
+  }
+
+  &__item {
+    color: rgba(0, 0, 0, 0.6);
+    font-size: 1.25rem;
+    font-weight: 700;
+    line-height: normal;
+  }
+}
+</style>
