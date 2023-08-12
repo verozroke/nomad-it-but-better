d5bb9268b82ebe6959d9db93f1b408b915948e64
Sabyrzhan Kadir <sab.sabyr@gmail.com>
2023-08-12 16:24:13 +0500

    adaptive

--- src/views/home/stages/HomeStages.vue
+++ src/views/home/stages/HomeStages.vue
@@ -32,6 +32,33 @@
         <div class="stages__text">После слаженной и комплексной работы наших сотрудников вы получаете готовый проект с продуманным дизайном, грамотным маркетингом и безупречной технической работы наших программистов</div>
       </div>
     </div>
+    <div class="stages__main__listversion">
+      <div class="stages__bignum">1</div>
+      <div class="stages__textblock">
+        <div class="stages__rainbowtitle">Связь с нами</div>
+        <div class="stages__text">После связи с нами и ознакомления с вашим продуктом или услугой мы организуем созвон</div>
+      </div>
+      <div class="stages__bignum">2</div>
+      <div class="stages__textblock">
+        <div class="stages__rainbowtitle">Обсуждение проекта</div>
+        <div class="stages__text">Во время нашего созвона мы задаем вам вопросы чтобы получить всю необходимую и важную информацию о вашем бизнесе и лучше понять его цели и задачи.</div>
+      </div>
+      <div class="stages__bignum">3</div>
+      <div class="stages__textblock">
+        <div class="stages__rainbowtitle">Подключение программистов / дизайнеров / маркетологов</div>
+        <div class="stages__text">После получения всей необходимой информации, мы распределяем работу между нашей командой для эффективного и планомерного выполнения задач.</div>
+      </div>
+      <div class="stages__bignum">4</div>
+      <div class="stages__textblock">
+        <div class="stages__rainbowtitle">Процесс разработки</div>
+        <div class="stages__text">Перед началом работ мы устанавливаем сроки, в которых подробно описываем все этапы процесса разработки.</div>
+      </div>
+      <div class="stages__bignum">5</div>
+      <div class="stages__textblock">
+        <div class="stages__rainbowtitle">Готовый проект</div>
+        <div class="stages__text">После слаженной и комплексной работы наших сотрудников вы получаете готовый проект с продуманным дизайном, грамотным маркетингом и безупречной технической работы наших программистов</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -47,8 +74,8 @@
 
   .stages__title {
     h2 {
-      font-family: Inter;
-      font-size: 3.125rem;
+      font-family: Inter, sans-serif;
+      font-size: 2.5rem;
       font-style: normal;
       font-weight: 700;
       line-height: normal;
@@ -56,7 +83,7 @@
 
     h3 {
       opacity: 0.4;
-      font-family: Inter;
+      font-family: Inter, sans-serif;
       font-size: 1.875rem;
       font-style: normal;
       font-weight: 700;
@@ -97,7 +124,7 @@
     }
 
     .stages__rainbowtitle {
-      font-family: 'Inter';
+      font-family: 'Inter', sans-serif;
       font-size: 2rem;
       font-style: normal;
       font-weight: 400;
@@ -118,4 +145,111 @@
     }
   }
 }
+
+.stages__main__listversion {
+  display: none;
+  grid-template-columns: auto auto;
+}
+
+@media only screen and (max-width: 900px) {
+  .stages__main {
+    display: none !important;
+  }
+
+  .stages__main__listversion {
+    display: static;
+
+    display: grid;
+    grid-template-columns: auto auto;
+    justify-content: center;
+    column-gap: 30px;
+
+
+    .stages__line {
+      grid-column: 2;
+      grid-row: 1 / span 5;
+      background: white;
+      width: 1px;
+    }
+
+    .stages__bignum {
+      font-family: Archivo Black;
+      font-size: 8rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      opacity: 0.2;
+
+
+      &.left {
+        text-align: right;
+      }
+    }
+
+    .stages__rainbowtitle {
+      font-family: 'Inter', sans-serif;
+      font-size: 1.5rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+      max-width: 400px;
+
+      margin-top: 20px;
+      margin-bottom: 4px;
+
+      background: linear-gradient(167deg, #000C99 0%, #009234 100%);
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+
+    .stages__text {
+      width: 400px;
+      font-size: 1rem;
+    }
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  .stages {
+    padding-top: 10rem;
+    padding-left: 2rem;
+    padding-right: 2rem;
+  }
+
+  .stages__main__listversion {
+    width: 100%;
+
+    .stages__rainbowtitle, .stages__text {
+      width: 300px;
+    }
+  }
+}
+
+@media only screen and (max-width: 450px) {
+  .stages__title {
+    h2 {
+      font-size: 1.5rem !important;
+    }
+
+    h3 {
+      font-size: 1rem !important;
+    }
+  }
+  .stages__main__listversion {
+
+    .stages__bignum {
+      font-size: 4.5rem;
+    }
+
+    .stages__rainbowtitle {
+      margin-top: 5px;
+    }
+
+    .stages__text, .stages__rainbowtitle {
+      width: 200px;
+    }
+  }
+}
+
 </style>
